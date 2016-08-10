00ee4c548e573baa155dafd67ff9948d16fb6200
Bobby Crowston and Seth Kendall <seth@>
2016-08-10 14:05:45 -0700

    initial commit for appointments form page

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var dateInput = $("input#date").val();
+    var startInput = $("input#start").val();
+    var endInput = $("input#end").val();
+
+    $(".name").text(nameInput);
+    $(".date").text(dateInput);
+    $(".start").text(startInput);
+    $(".end").text(endInput);
+
+    $("#confirmation").show();
+    event.preventDefault();
+  });
+});
